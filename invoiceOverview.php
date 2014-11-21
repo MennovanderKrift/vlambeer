0da7b40a10f9aee9a0b9976f1c8721616ed1ca77
mikeo26 <mikeo26@gmail.com>
2014-11-21 11:52:32 +0100

    factuur overzicht

---
+++ invoiceOverview.php
@@ -0,0 +1,44 @@
+<?php 
+
+// include to header file goes here
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Invoice overview</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	
+
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<td>Invoice #</td>
+				<td>Invoice date</td>
+				<td>Download</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>#201411001</td>
+				<td>21-11-2014</td>
+				<td><a href="#">x</a></td>
+			</tr>
+			<tr>
+				<td>#201411002</td>
+				<td>21-11-2014</td>
+				<td><a href="#">x</a></td>
+			</tr>
+		</tbody>
+	</table>
+
+</div>
+	
+</body>
+</html>
