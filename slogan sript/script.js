a6b7cc029f3e1a0d2ce49538cd56021ea8fed7bb
regilio dielemans <regilio_dielemans@hotmail.com>
2014-11-21 15:03:23 +0100

    spel fout

--- slogan sript/script.js
+++ slogan sript/script.js
@@ -2,4 +2,4 @@ function random(min, max){
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-document.getElementById("slogan").innerHTML = "bringing back arcard game since " + random(1725, 2014);
+document.getElementById("slogan").innerHTML = "Bringing back arcade since " + random(1725, 2014);
