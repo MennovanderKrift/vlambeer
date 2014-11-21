3c893a26dcdaf74cdcc8e0d766ddfcbd0a109cb4
koentjeh <koen-debont@hotmail.com>
2014-11-21 14:11:59 +0100

    sales functie onderzocht

---
+++ Onderzoek sales functie/activate.php
@@ -0,0 +1,11 @@
+<?php
+  require 'Database.php';
+
+  $id = $_GET['id'];
+
+  if ( isset($_GET['id']) ) {
+    $sql = "UPDATE salesfunctie SET sale = 0 WHERE id = '$id'";
+    $stmt = $db->query($sql);
+    header('location: index.php');
+  }
+?>
