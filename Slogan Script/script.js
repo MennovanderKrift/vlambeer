dbd3cf2a46ff5c05449d45177a057363da951834
Menno van der Krift (Laptop) <menno.vd.gmail.com>
2014-11-24 14:54:54 +0100

    Debugging GitHub .... again

--- Slogan Script/script.js
+++
@@ -1,5 +0,0 @@
-function random(min, max){
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-document.getElementById("slogan").innerHTML = "Bringing back arcade since " + random(1725, 2014);
