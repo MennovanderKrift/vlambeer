73c6df0d85e2032f9ff7e36aabdf5d977cb8ea27
mikeo26 <mikeo26@gmail.com>
2014-11-24 15:09:19 +0100

    User registratie klaar

---
+++ userRegistration/addUser.php
@@ -0,0 +1,26 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'vlambeer');
+
+$username		=		$_POST['username'];
+$email			=		$_POST['email'];
+$password 		=		$_POST['password'];
+$password2		=		$_POST['password2'];
+
+if(isset($_POST['register'])) {
+	if($password != $password2) {
+		header("location: index.php?msg=pe1");
+	}elseif(!(strlen($_POST['password']) >= 8)) {
+		header("location: index.php?msg=pe2");
+	}elseif(mysqli_num_rows(mysqli_query($con, "SELECT * FROM users WHERE username = '$username'"))) {
+		header("location: index.php?msg=ue1");
+	}elseif(mysqli_num_rows(mysqli_query($con, "SELECT * FROM users WHERE email = '$email'"))) {
+		header("location: index.php?msg=ee1");
+	} else {
+		$hashedPass = password_hash("$password", PASSWORD_DEFAULT);
+		$query = mysqli_query($con, "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashedPass')");
+		header("location: index.php?msg=sr1");
+	}
+}
+
+?>
