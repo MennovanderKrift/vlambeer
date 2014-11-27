1ab70f4a1f596b801ff30f1483a0aaa9ad5cf80a
koentjeh <koen-debont@hotmail.com>
2014-11-27 09:23:41 +0100

    wireframes

--- coderen/include/header.php
+++ coderen/include/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <title>Vlambeer</title>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
 
   <link rel="stylesheet" href="assets/style.css">
 
@@ -9,7 +11,5 @@
   <meta name="description" content="Vlambeer webstore">
   <meta name="keywords" content="vlambeer, games, action, schooter, shirt, webshop">
   <meta name="author" content="Mike Oerlemans, Koen de Bont, Friso Kin, Regilio Dielemans, Menno van der Krift, Daniel van Bavel">
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
