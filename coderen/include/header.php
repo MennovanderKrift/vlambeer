fde4ab934038b950676bff1e83b9ff475bd2f1d7
Menno van der Krift (Laptop) <menno.vd.gmail.com>
2014-11-27 09:34:05 +0100

    Header bijgewerkttt.

--- coderen/include/header.php
+++ coderen/include/header.php
@@ -3,13 +3,13 @@
 <head>
   <title>Vlambeer</title>
 <<<<<<< HEAD
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../assets/img/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="../assets/img/favicon.ico" type="image/x-icon">
 
 =======
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 >>>>>>> f91dd2650f81379af6aa33e2f614d0dd20eaad17
-  <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
   <meta charset="UTF-8">
   <meta name="description" content="Vlambeer webstore">
