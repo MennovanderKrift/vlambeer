abdd4793c90aeba55e28fa7d74f7cbc940d14370
koentjeh <koen-debont@hotmail.com>
2014-11-27 11:09:25 +0100

    index + css

--- coderen/index.php
+++ coderen/index.php
@@ -3,23 +3,28 @@
 ?>
 	<header>
 		<div class="header-top">
-
+			click to store / zoeken
 		</div>
 		<div class="header-calltoaction">
-
+			<h1>Logo</h1>
 		</div>
 		<div class="header-menu">
-
+			menu1 | menu2 | menu3 | menu4
 		</div>
 	</header>
 	<section>
 		<div class="container">
-
+			<div class="slider">
+				<h1>slider | gameinfo (linksonder)</h1>
+			</div>
+			<div class="info">
+				<h1>info bedrijf</h1>
+			</div>
 		</div>
 	</section>
 	<footer>
 		<div class="footer">
-			
+			Vlambeer is a Dutch independent Game Studio | twitter | info / Friends of Vlambeer
 		</div>
 	</footer>
 </body>
