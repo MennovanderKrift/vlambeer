71f1a534f70d71cb33a323dc0afc294ec53dbb22
Menno van der Krift (Laptop) <menno.vd.gmail.com>
2014-11-27 11:15:31 +0100

    Store index.php
    Store index.php

--- coderen/store/index.php
+++ coderen/store/index.php
@@ -2,11 +2,13 @@
 	<h1> Vlambeer Games & Social media icons</h1>
 	<header>
 		<div class="store-header-top">
-		<h1>Logo & slider</h1>
+		<h1> .store-header-top</h1>
+			<div class='store-logo'> <h1>Logo area</h1> </div>
+			<div class='store-slider'><h1>Store-slider area</h1></div>
 		</div>
 
 		<div class="store-header-menu">
-			<ul>
+			<ul> Store Menu
 				<li class='active'>Menu</li>
 				<li>Games</li>
 				<li>Bundles</li>
@@ -20,8 +22,39 @@
 	<section>
 		<div class="store-container">
 			<h1>.store-container</h1>
+			<div class='store-items-wrapper'>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+			</div>
 		</div>
 	</section>
+
+	<section>
+		<div class="store-container">
+			<h1>.store-container</h1>
+			<div class='store-items-wrapper'>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="store-container">
+			<h1>.store-container</h1>
+			<div class='store-items-wrapper'>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+				<div class='store-item'></div>
+			</div>
+		</div>
+	</section>
+
+	<div class='store-about'>
+		<h1>.store-about</h1>
+	</div>
 
 	<footer>
 		<div class="store-footer" cols='md-6'>
