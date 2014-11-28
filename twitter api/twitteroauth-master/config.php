4703ceac40259a750d45e367709f7317868f5bac
regilio dielemans <regilio_dielemans@hotmail.com>
2014-11-28 13:28:43 +0100

    geen idee

---
+++ twitter api/twitteroauth-master/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '3nOvwiVmqPRyW5zTeKj1p3KCX');
+define('CONSUMER_SECRET', 'WwGlUfDjBXA65s1LUTlVqHyCuiC3GSj6AWB5E1DkplHovTNf08	');
+define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
