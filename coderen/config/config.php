090306418fb920a7acb9f4740877cc109ec75265
DanielvanBavel <D179143@edu.rocwb.nl>
2014-11-28 09:44:24 +0100

    dasf

--- coderen/config/config.php
+++ coderen/config/config.php
@@ -1,16 +1,14 @@
 <?php 
-
 define('USER', 'root');
 define('PASS', '');
 define('DSN', 'mysql:host=localhost;dbname=vlambeer');
 
-try {
+try 
+{
 	$db = new PDO(DSN, USER, PASS);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-	echo $e->getMessage();
-	
-}
-
-?>
+} 
+catch (PDOException $e) 
+{
+	echo $e->getMessage();	
+}?>
