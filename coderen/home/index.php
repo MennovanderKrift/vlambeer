001b3ea279071d5e4e1821a2d6c3a21ca087a2bc
koentjeh <koen-debont@hotmail.com>
2014-11-28 09:51:58 +0100

    doorsturen naar index

---
+++ coderen/home/index.php
@@ -0,0 +1,40 @@
+<?php
+  include 'include/header.php';
+?>
+	<header>
+		<div class="header-top">
+			<a href="store/index.php">click to store</a> / zoeken
+		</div>
+		<div class="header-calltoaction">
+			<div class="logo-slogan">
+				<img src="assets/img/vlambeer-logo.png">
+				<h1 class="header-title">Vlambeer</h1>
+				<p id="slogan" class="slogantext"></p>
+			</div>
+		</div>
+		<div class="header-menu">
+			menu1 | menu2 | menu3 | menu4
+		</div>
+	</header>
+	<section>
+		<div class="container">
+			<div class="newsitem">
+
+			</div>
+			<div class="slider">
+				<h1>slider | gameinfo (linksonder)</h1>
+			</div>
+			<div class="info">
+				<h1>info bedrijf</h1>
+			</div>
+			
+		</div>
+	</section>
+	<footer>
+		<div class="footer">
+			Vlambeer is a Dutch independent Game Studio | twitter | info / Friends of Vlambeer
+		</div>
+	</footer>
+<?php
+	require 'include/footer.php';
+?>
