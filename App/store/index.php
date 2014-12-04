b567e8b487525a7c4d8f10783f9c9f066e7ef5be
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-04 10:16:25 +0100

    css

--- App/store/index.php
+++ App/store/index.php
@@ -3,111 +3,21 @@
 
 	include '../includes/head.php';
 ?>
-		<div class="header-calltoaction">
-			<div class="logo-slogan">
-				<img src="../assets/img/vlambeer-logo.png">
-				<h1 class="header-title">Vlambeer</h1>
-				<p id="slogan" class="slogantext"></p>
-			</div>
-		</div>
+<div class="container">
+	<div class="index-header">
+		hier zit die
+	</div>
+</div>
+
 <?php
 	include '../includes/menu.php';
 ?>
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner" role="listbox">
-		    <div class="item active">
-		      <img src="..." alt="...">
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    <div class="item">
-		      <img src="..." alt="...">
-		      <div class="carousel-caption">
-		        ...
-		      </div>
-		    </div>
-		    ...
-		  </div>
-
-		  <!-- Controls -->
-		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		    <span class="sr-only">Previous</span>
-		  </a>
-		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		    <span class="sr-only">Next</span>
-		  </a>
-	<h1> Vlambeer Games & Social media icons | <a href="login.php">login</a><form action="../controllers/authController.php" method="POST"><input type="submit" value="logout" name="logout"></form></h1>
-	<header>
-		<div class="store-header-top">
-		<h1> .store-header-top</h1>
-			<div class='store-logo'> <h1>Logo area</h1> </div>
-			<div class='store-slider'><h1>Store-slider area</h1></div>
-		</div>
-
-		<div class="store-header-menu">
-			<ul> Store Menu
-				<li class='active'>Menu</li>
-				<li>Games</li>
-				<li>Bundles</li>
-				<li>Clothes</li>
-				<li>Music</li>
-				<li>van Vlambeer Games</li>
-			</ul>
-		</div>
-	</header>
-
-	<section>
-		<div class="store-container">
-			<h1>.store-container</h1>
-			<div class='store-items-wrapper'>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-			</div>
-		</div>
-	</section>
-
-	<section>
-		<div class="store-container">
-			<h1>.store-container</h1>
-			<div class='store-items-wrapper'>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-			</div>
-		</div>
-	</section>
-
-	<section>
-		<div class="store-container">
-			<h1>.store-container</h1>
-			<div class='store-items-wrapper'>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-				<div class='store-item'></div>
-			</div>
-		</div>
-	</section>
-
-	<div class='store-about'>
-		<h1>.store-about</h1>
+<div class="container">
+	<div class="index-head">
+		hier zit die2
 	</div>
+</div>
 
-	<footer>
-		<div class="store-footer">
-			<h1>.store-footer</h1>
-		</div>
 <?php
 	include '../includes/footer.php';
 ?>
