5bdd3db2af8074498ac504f1ae06210361b95e0c
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-08 16:13:36 +0100

    items toegevoegd index

--- App/store/index.php
+++ App/store/index.php
@@ -81,6 +81,73 @@ require '../includes/header.php';
 			<div class="arrow-down"></div>
 		</div>
 	</div>
+
+
+	<div class="index-items col-md-12">
+		<h2><center>miscellaneous</center></h2>
+		<div class="col-md-3">
+			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+	</div>
+
+
+		<div class="index-items col-md-12">
+		<h2><center>music</center></h2>
+		<div class="col-md-3">
+			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+	</div>
+
+	<div class="index-items col-md-12">
+		<h2><center>non-vlambeer games</center></h2>
+		<div class="col-md-3">
+			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="arrow-down"></div>
+		</div>
+	</div>
+	</div>	
 </div>
 
 <?php
