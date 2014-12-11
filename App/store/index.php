be4239b1ad8a6b366b4a35b6dcd1fbfab38fbbe3
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-11 11:16:06 +0100

    homepage

--- App/store/index.php
+++ App/store/index.php
@@ -1,8 +1,5 @@
 <?php 
-
 require '../includes/header.php';
-
-
 ?>
 
 <div class="index-slider">
@@ -15,28 +12,28 @@ require '../includes/header.php';
 <div class="backgroundcolor1-index">
 	<div class="container">
 		<div class="index-items col-md-12">
-			<h2><center>games</center></h2>
+			<h2>games</h2>
 			<div class="col-md-3">
-				<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<a href="games/CodAW.php?id=1"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
+				<div class="product-info"><p>Serious Sam: <br>The Random Encounter <br>(PC, Steam)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
 				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-info"><p>Super Crate Box <br> (PC, Mac)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
 				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-info"><p>Super Crate Box <br> (iOS)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
 				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-info"><p>Super Crate Box <br> (Vita)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 		</div>
@@ -44,16 +41,16 @@ require '../includes/header.php';
 </div>
 <div class="container">
 	<div class="index-items col-md-12">
-		<h2><center>bundles</center></h2>
+		<h2>bundles</h2>
 		<div class="col-md-3">
-			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Vlambeer’s <br> Crate Box Bundle</p></div>
 			<div class="arrow-down"></div>
 		</div>
 
 		<div class="col-md-3">
 			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-info"><p>Luftrausers <br> Limited Edition</p></div>
 			<div class="arrow-down"></div>
 		</div>
 	</div>
@@ -62,28 +59,28 @@ require '../includes/header.php';
 	<div class="container">
 
 		<div class="index-items col-md-12">
-		<h2><center>clothes</center></h2>
+		<h2>clothes</h2>
 			<div class="col-md-3">
-				<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-img"><img src="../assets/img/tshirt1.png" alt=""></div>	
+				<div class="product-info"><p>Luftrausers <br> Mens & ladies T-shirt</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
-				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-img"><img src="../assets/img/tshirt2.png" alt=""></div>	
+				<div class="product-info"><p>Vlambeer Logo <br> T-shirt</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
-				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-img"><img src="../assets/img/tshirt3.png" alt=""></div>	
+				<div class="product-info"><p>Dancing Vlambeer <br> T-shirt</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
-				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-img"><img src="../assets/img/tshirt4.png" alt=""></div>	
+				<div class="product-info"><p>Gund Godz <br> T-shirt</p></div>
 				<div class="arrow-down"></div>
 			</div>
 		</div>
@@ -91,16 +88,16 @@ require '../includes/header.php';
 </div>
 <div class="container">
 	<div class="index-items col-md-12">
-		<h2><center>miscellaneous</center></h2>
+		<h2>miscellaneous</h2>
 		<div class="col-md-3">
-			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>
+			<div class="product-info"><p>Vlambeer <br> Enemies Poster</p></div>
 			<div class="arrow-down"></div>
 		</div>
 
 		<div class="col-md-3">
 			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-info"><p>Super Crate Box <br> Plushie</p></div>
 			<div class="arrow-down"></div>
 		</div>
 	</div>
@@ -108,22 +105,22 @@ require '../includes/header.php';
 <div class="backgroundcolor1-index">
 	<div class="container">
 		<div class="index-items col-md-12">
-			<h2><center>music</center></h2>
+			<h2>music</h2>
 			<div class="col-md-3">
-				<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-img"><img src="../assets/img/music1.png" alt=""></div>
+				<div class="product-info"><p>Vlambeer <br> Original Soundtrack <br> (Physical)</p></div>
+				<div class="arrow-down"></div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="product-img"><img src="../assets/img/music2.png" alt=""></div>	
+				<div class="product-info"><p>Vlambeer <br> Original Soundtrack <br> (Digital)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
 			<div class="col-md-3">
 				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
-				<div class="arrow-down"></div>
-			</div>
-
-			<div class="col-md-3">
-				<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+				<div class="product-info"><p>Super Crate Box <br> (Digital)</p></div>
 				<div class="arrow-down"></div>
 			</div>
 
@@ -132,28 +129,28 @@ require '../includes/header.php';
 </div>
 <div class="container">
 	<div class="index-items col-md-12">
-		<h2><center>non-vlambeer games</center></h2>
+		<h2>non-vlambeer games</h2>
 		<div class="col-md-3">
-			<a href="games/CodAW.php"><div class="product-img"><img src="../assets/img/puppy.png" alt=""></div></a>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
+			<div class="product-info"><p>Radical Fishing <br> via bored.com</p></div>
 			<div class="arrow-down"></div>
 		</div>
 
 		<div class="col-md-3">
 			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-info"><p>Dinosaur Zookeeper <br> via Adult Swim</p></div>
+			<div class="arrow-down"></div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="product-img"><img src="../assets/img/karate.png" alt=""></div>	
+			<div class="product-info"><p>Karate <br> (PC) <br> via Not Vlambeer</p></div>
 			<div class="arrow-down"></div>
 		</div>
 
 		<div class="col-md-3">
 			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
-			<div class="arrow-down"></div>
-		</div>
-
-		<div class="col-md-3">
-			<div class="product-img"><img src="../assets/img/puppy.png" alt=""></div>	
-			<div class="product-info"><p>Serious Sam:The Random Encounter (PC) (Steam)</p></div>
+			<div class="product-info"><p>Super Puppy Boy <br> (PC) <br> via Not Vlambeer</p></div>
 			<div class="arrow-down"></div>
 		</div>
 	</div>
