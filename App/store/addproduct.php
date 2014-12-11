f89ef42ebc169ad251be17b7a6bae08705200e67
mikeo26 <mikeo26@gmail.com>
2014-12-11 09:25:04 +0100

    addproduct

--- App/store/addproduct.php
+++ App/store/addproduct.php
@@ -2,32 +2,30 @@
 require '../config/Database.php';
 session_start();
 
-if(isset($_GET['id'])) 
-{
+if(isset($_GET['id'])) {
 	$id = $_GET['id'];
 }
 
 $quantity = isset($_GET['quantity']) ? $_GET['quantity'] : "";
 
-if (isset($_GET['name'])) 
-{
-	$name = $_GET['name'];
-}
+// if (isset($_GET['name'])) {
+// 	$name = $_GET['name'];
+// }
 
-$cart_item = array('name' => $name, 'quantity' => $quantity);
+$cart_item=array(
+	'name'=>$name,
+	'quantity'=>$quantity
+);
 
-if(!isset($_SESSION['cart_items'])) 
-{
+if(!isset($_SESSION['cart_items'])) {
 	$_SESSION['cart_items'] = array();
 }
 
-if(array_key_exists($id, $_SESSION['cart_items'])) 
-{
+if(array_key_exists($id, $_SESSION['cart_items'])) {
 	header("location: shoppingcart.php?action=exists&id=" . $id);
-}
-else 
-{
+}else {
 	$_SESSION['cart_items'][$id] = $name;
 	header("location: shoppingcart.php?action=added&id=" . $id);
 }
+
 ?>
