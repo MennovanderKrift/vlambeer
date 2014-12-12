81a142ece0367a071c2938181dafda423e0d00f2
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-12 10:11:20 +0100

    serious sam page

--- App/store/games/serious-sam.php
+++ App/store/games/serious-sam.php
@@ -1,10 +1,27 @@
 <?php 
 require '../../includes/storeGameHeader.php';
 ?>
+<div class="backgroundcolor-seriuos-sam1">
+	<div class="container">
+			<div class="col-md-3">
+				<div class="product-img"><img src="../../assets/img/ss_logo.png" alt=""></div>	
+				<div class="product-info"><p>Serious Sam: <br>The Random Encounter <br>(PC, Steam)</p></div>
+				<div class="arrow-down"></div>
+			</div>
+	</div>
+</div>
 
+<div class="backgroundcolor-seriuos-sam2">
+	<div class="container">
 
+	</div>
+</div>
 
-
+<div class="backgroundcolor-seriuos-sam3">
+	<div class="container">
+	
+	</div>
+</div>
 <?php
 require '../../includes/footer.php'; 
 ?>
