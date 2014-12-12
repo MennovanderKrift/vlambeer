2fa502a1207f696cb8945c132b134094380211bb
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-12 12:03:48 +0100

    ja
    ja

--- App/store/games/serious-sam.php
+++ App/store/games/serious-sam.php
@@ -3,23 +3,36 @@ require '../../includes/storeGameHeader.php';
 ?>
 <div class="backgroundcolor-seriuos-sam1">
 	<div class="container">
-			<div class="col-md-3">
-				<div class="product-img"><img src="../../assets/img/ss_logo.png" alt=""></div>	
-				<div class="product-info"><p>Serious Sam: <br>The Random Encounter <br>(PC, Steam)</p></div>
+			<div class="col-md-4">
+				<div class="product-img-ss"><img src="../../assets/img/ss_logo.png" alt=""></div>	
+				<div class="product-info"><p>Kies je wapen en richt op het <br>meest willekeurige Serious <br>Sam-avontuur tot nu toe!</p></div>
 				<div class="arrow-down"></div>
 			</div>
+			<div class="col-md-8">
+				<img class="slider-serious-sam" src="../../assets/img/ss_afb1.png" alt="">
+			</div>
+
 	</div>
 </div>
 
 <div class="backgroundcolor-seriuos-sam2">
 	<div class="container">
-
+		<div class="col-md-4">
+			<img src="../../assets/img/ss_afb2.png" alt="">
+		</div>
+		<div class="col-md-8">
+			<h3>lorem ipson</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est fugit velit repellat nulla distinctio quidem dolor rerum, error pariatur consectetur minus quam odit natus aliquid vero debitis iure doloribus id.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis aliquid, similique error consectetur ut consequuntur expedita, corrupti corporis ipsam aperiam facilis omnis quidem perspiciatis, non velit laudantium nesciunt consequatur amet.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat natus, necessitatibus quos reiciendis dolores dignissimos, aperiam rem. Facilis doloribus nesciunt, quam incidunt laboriosam excepturi libero sed, vel vero, velit quisquam.</p>
+			
+		</div>
 	</div>
 </div>
 
 <div class="backgroundcolor-seriuos-sam3">
 	<div class="container">
-	
+
 	</div>
 </div>
 <?php
