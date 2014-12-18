39cc9ba5f3a068808b931c99941fa9f48b1736ca
regilio dielemans <regilio_dielemans@hotmail.com>
2014-12-18 12:13:43 +0100

    merchandise

--- App/store/merchandise.php
+++ App/store/merchandise.php
@@ -9,7 +9,20 @@
 		<p><h3>prijzen</h3><br>mannen: 19.95 <br>vrouwen: 17.95</p>
 	</div>
 	<div class="col-md-3 shirt-maat">
-		<p><a href="addproduct.php?id=<?php echo $_GET['id'] ?>" class="btn">In winkelwagen</a></p>
+		<div class="select-size">
+			<p>selecteer uw maat
+			<select>
+	  			<option>s</option>
+	  			<option>m</option>
+	  			<option>l</option>
+			</select>
+			</p>
+		</div>
+		
+
+		<div class="insert-cart">
+			<a href="addproduct.php?id=<?php echo $_GET['id'] ?>" class="btn">In winkelwagen</a>
+		</div>
 	</div>
 </div>
 <?php require '../includes/footer.php'; ?>
