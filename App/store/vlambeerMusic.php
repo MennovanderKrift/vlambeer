c6b7cb43c9f4d9cb1e7d07d1f5447fdcb9bc5760
frisokin <d194188@edu.rocwb.nl>
2015-01-06 15:04:53 +0100

    music update / foto's toegevoegd

---
+++ App/store/vlambeerMusic.php
@@ -0,0 +1,9 @@
+<?php require '../includes/header.php' ?>
+
+<div class="container">
+
+<img class="slider-serious-sam" src="../design/images/vlambeerMusic.jpeg" alt="">
+
+</div>
+
+<?php require '../includes/footer.php' ?>
