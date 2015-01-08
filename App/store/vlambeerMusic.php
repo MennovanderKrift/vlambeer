8a330eae7ae7b9cb199381c499fd2e0f259f74eb
frisokin <d194188@edu.rocwb.nl>
2015-01-08 12:39:01 +0100

    music update

--- App/store/vlambeerMusic.php
+++ App/store/vlambeerMusic.php
@@ -16,7 +16,52 @@
 
 
 <div class="musicInfo">
+<div class="musicInfo-header"><h2 class="musicInfo-title">The Vlambeer Original Soundtrack</h2> </div>
+	<p>
+		Few Indie Game Devs get their paws wet with as many composers and artists as Vlambeer does, and the logical conclusion is this, the Vlambeer Original Soundtrack. 
+		Comprised of compositions from a variety of artists across a multitude of games, it’s the perfect collection for any gaming, music, or Vlambeer fan.
+	</p>	
 
+<ul class="list1">
+<li>1. GG Main Theme</li>										
+<li>2. RF Theme</li>
+<li>3. DZK Calm Seas</li>
+<li>4. DZK Menu Theme</li>
+<li>5. DZK Week #1</li>
+<li>6. DZK Week #2</li>
+<li>7. DZK Week #3</li>
+<li>8. DZK Night Theme</li>
+<li>9. LR Theme</li>
+<li>10. SCB Theme</li>
+<li>11. SCB Tutorial</li>
+<li>12. SCB Construction Ya</li>
+<li>13. SCB Rocket Silo</li>
+<li>14. SCB Moon Temple</li>
+<li>15. SCB Elevator Music</li>
+<li>16. LRS Demo 1</li>
+<li>17. LRS Demo 2</li>
+<li>18. LRS Demo 3</li>
+<li>19. SCB Interlude</li>
+</ul>
+<ul class="list2">
+<li>20. YH Day</li>
+<li>21. YH Night</li>
+<li>22. SS:TRE Menu Theme</li>
+<li>23. SS:TRE Desert Theme</li>
+<li>24. SS:TRE Temple Theme</li>
+<li>25. SS:TRE Pyramid Theme</li>
+<li>26. SS:TRE Boss Theme</li>
+<li>27. RF Radical Fishing</li>
+<li>28. GG Venus Supermax</li>
+<li>29. GG Sewers</li>
+<li>30. GG Boss Battle #1</li>
+<li>31. GG Basement</li>
+<li>32. GG Hotel Lobby</li>
+<li>33. GG Boss Battle #2</li>
+<li>34. SCB Early Test</li>
+<li>35. SCB Construction Yard (Kozilek Remix)</li>
+<li>36. GG Phonecall</li>
+</ul>
 </div>
 
 
