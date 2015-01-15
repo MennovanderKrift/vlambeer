fec008cf97e7e4d3b4a559224b098e5dfb88b065
Menno van der Krift (Laptop) <menno.vd.gmail.com>
2015-01-15 09:21:09 +0100

    Typfout in css gefixed -.-; newsletter.php

--- App/admin/newsletter.php
+++ App/admin/newsletter.php
@@ -11,9 +11,9 @@ if ($_SESSION['role'] == 'admin') {
 	<div class="admin-title">Write A New Newsletter</div>
 
 		<form action="../controllers/adminController.php" class="newsletter-form" method="POST">
-			<label for="title">Title</label>
+			<label for="newsletter_title">Title</label>
 				<input type="text" name="newsletter_title" id="newsletter_title">
-			<label for="content">Content</label>
+			<label for="newsletter_content">Content</label>
 				<textarea name="newsletter_content" id="newsletter_content" cols="30" rows="10"></textarea>
 				<input type="submit" name="sendNewsletter" value="Send" class="btn sendNewsletter-button">
 		</form>
