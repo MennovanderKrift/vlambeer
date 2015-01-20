f79adbb60a830589a5ef078486066655814d2fc4
Menno (Desktop) <d174880@edu.rocwb.nl>
2015-01-20 20:10:46 +0100

    Database update (product size added)

--- App/vlambeer (updated 20-1-2015).sql
+++ App/vlambeer (updated 20-1-2015).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2015 at 05:51 PM
+-- Generation Time: Jan 20, 2015 at 08:10 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -17,23 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `new_vlambeer`
+-- Database: `vlambeer`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_carts`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_carts` (
-  `customer_id` int(10) NOT NULL,
-  `cart_id` int(20) NOT NULL,
-  `product_id` int(20) NOT NULL,
-  `quantity` int(20) NOT NULL,
-  `item_price` float NOT NULL,
-  PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -56,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `tbl_customers` (
   `news_letter` tinyint(1) NOT NULL,
   PRIMARY KEY (`customer_id`),
   UNIQUE KEY `customer_id` (`customer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `tbl_customers`
@@ -68,7 +53,8 @@ INSERT INTO `tbl_customers` (`customer_id`, `username`, `password`, `name`, `las
 (4, 'koekwaus', '$2y$10$.Epjc.Q4HsVm2bO5EALQQ.Vq0TTldXDDy4kAH4K5AKUF.V9nf7usm', '', '', 'male', '', 0, '', '', 'koekwaus@vlambeer.nl', 0),
 (5, 'flapdrol123', '$2y$10$WNoiXKeydMrRfbYZiAhLJO5Ez4l4bZSrW/L/Jw7dL3HZ/0ZarwcuO', '', '', 'male', '', 0, '', '', 'flapdrol123@vlambeer.nl', 0),
 (6, 'supermario', '$2y$10$Qemk2FNuFGuTm9VcXLYmIO3Wn1uBNzio31h43rXU9NrxBzjg5mzr2', '', '', 'male', '', 0, '', '', 'supermario@nintendo.nl', 0),
-(7, 'megaman', '$2y$10$5FzAHr4Z00nBze81mXVl0eVNai1qTXuBmLE5fl9CCpA0VxSJWO3Jy', 'Mega', 'Man', 'male', '', 0, '4848aa', '0612345678', 'megaman@capcom.nl', 1);
+(7, 'peach', '$2y$10$5FzAHr4Z00nBze81mXVl0eVNai1qTXuBmLE5fl9CCpA0VxSJWO3Jy', 'peach', 'Toadstool', 'male', '', 0, '4848aa', '0612345678', 'peach@nintendo.com', 1),
+(8, 'megaman', '$2y$10$Zmf/2fwNdiGWnU6wfc7I8.Ku.nPMYEtZWHp0/fk8xRJPev1wA/8mS', '', '', 'male', '', 0, '', '', 'megaman@capcom.nl', 0);
 
 -- --------------------------------------------------------
 
@@ -136,42 +122,29 @@ CREATE TABLE IF NOT EXISTS `tbl_products` (
   `name` varchar(255) NOT NULL COMMENT 'Product name',
   `description` longtext NOT NULL COMMENT 'Product description',
   `item_price` float NOT NULL COMMENT 'Product price',
+  `size` varchar(3) NOT NULL,
   `category` varchar(60) NOT NULL COMMENT 'Product category',
   `image` varchar(255) NOT NULL,
   `image2` varchar(255) NOT NULL COMMENT 'Secondary product image',
   `stock` int(11) NOT NULL,
   PRIMARY KEY (`product_id`),
   UNIQUE KEY `product_id` (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `tbl_products`
 --
 
-INSERT INTO `tbl_products` (`product_id`, `name`, `description`, `item_price`, `category`, `image`, `image2`, `stock`) VALUES
-(1, 'Luftrauser T-shirt', 'Stylish LUFTRAUSERS T-shirt designed by Amon26', 18, 'Clothes', '../assets/img/tshirt-luftrausers.jpg', '', 10),
-(2, 'Vlambeer T-shirt', 'T-shirt with Vlambeer logo on chest', 17.99, 'Clothes', '../assets/img/tshirt-vlambeer.jpg', '', 10),
-(3, 'Vlambeer 1st Birthday T-shirt', 'Dancing Vlambeer T-shirt', 18, 'Clothes', '../assets/img/tshirt-dancing-vlambeer.jpg', '', 10),
-(4, 'GUN GODZ T-shirt', 'GUN GODZ T-shirt with butt.', 18, 'Clothes', '../assets/img/tshirt-gungodz-butt.jpg', '', 10),
-(5, 'Vlambeer''s Crate Box Bundle', 'Includes t-shirt, exclusive poster, Vlambeer OST, pins, most of our 2010-2012 games on custom USB-drive', 50, 'Bundles', '../assets/img/crate-box-bundle.jpg', '', 10),
-(6, 'Vlambeer OST (Physical)', '\n		Few Indie Game Devs get their paws wet with as many composers and artists as Vlambeer does, and the logical conclusion is this, the Vlambeer Original Soundtrack. \n		Comprised of compositions from a variety of artists across a multitude of games, it’s the perfect collection for any gaming, music, or Vlambeer fan.\n	\n\n<ul class="list1">\n<li>1. GG Main Theme</li>										\n<li>2. RF Theme</li>\n<li>3. DZK Calm Seas</li>\n<li>4. DZK Menu Theme</li>\n<li>5. DZK Week #1</li>\n<li>6. DZK Week #2</li>\n<li>7. DZK Week #3</li>\n<li>8. DZK Night Theme</li>\n<li>9. LR Theme</li>\n<li>10. SCB Theme</li>\n<li>11. SCB Tutorial</li>\n<li>12. SCB Construction Ya</li>\n<li>13. SCB Rocket Silo</li>\n<li>14. SCB Moon Temple</li>\n<li>15. SCB Elevator Music</li>\n<li>16. LRS Demo 1</li>\n<li>17. LRS Demo 2</li>\n<li>18. LRS Demo 3</li>\n<li>19. SCB Interlude</li>\n</ul>\n<ul class="list2">\n<li>20. YH Day</li>\n<li>21. YH Night</li>\n<li>22. SS:TRE Menu Theme</li>\n<li>23. SS:TRE Desert Theme</li>\n<li>24. SS:TRE Temple Theme</li>\n<li>25. SS:TRE Pyramid Theme</li>\n<li>26. SS:TRE Boss Theme</li>\n<li>27. RF Radical Fishing</li>\n<li>28. GG Venus Supermax</li>\n<li>29. GG Sewers</li>\n<li>30. GG Boss Battle #1</li>\n<li>31. GG Basement</li>\n<li>32. GG Hotel Lobby</li>\n<li>33. GG Boss Battle #2</li>\n<li>34. SCB Early Test</li>\n<li>35. SCB Construction Yard (Kozilek Remix)</li>\n<li>36. GG Phonecall</li>\n</ul>', 12.99, 'Music', '../assets/img/vlambeer-ost-physical.jpg', '../assets/img/vlambeer-ost-physical-2.jpg', 10),
-(7, 'Vlambeer OST (Digital)', 'Includes unlimited streaming via the free Bandcamp app, plus high-quality download in MP3, FLAC and more.\n\n<ul class="list1">\n<li>1. GUN GODZ - Main Theme</li>										\n<li>2. Ridiculios Fishing - Theme</li>\n<li>3. Ridiculios Fishing - Calm Seas</li>\n<li>4. Dinosaur Zookeeper - Menu Theme</li>\n<li>5. Dinosaur Zookeeper - Week #1</li>\n<li>6. Dinosaur Zookeeper - Week #2</li>\n<li>7. Dinosaur Zookeeper - Week #3</li>\n<li>8. Dinosaur Zookeeper - Night Theme</li>\n<li>9. LUFTRAUSERS - Theme</li>\n<li>10. Super Crate Box - Theme</li>\n<li>11. Super Crate Box - Tutorial</li>\n<li>12. Super Crate Box - Construction Yard</li>\n<li>13. Super Crate Box - Rocket Silo</li>\n<li>14. Super Crate Box - Moon Temple</li>\n<li>15. Super Crate Box - Elevator Music</li>\n<li>16. LUFTRAUSERS - Demo #1</li>\n<li>17. LUFTRAUSERS - Demo #2</li>\n<li>18. LUFTRAUSERS - Demo #3</li>\n<li>19. Super Crate Box - Interlude</li>\n<li>20. Yeti Hunter - Day</li>\n<li>21. Yeti Hunter - Night</li>\n</ul>\n\n<ul class="list2">\n<li>22. Serious Sam: The Random Encounter - Menu Theme</li>\n<li>23. Serious Sam: The Random Encounter - Desert Theme</li>\n<li>24. Serious Sam: The Random Encounter - Temple Theme</li>\n<li>25. Serious Sam: The Random Encounter - Pyramide Theme</li>\n<li>26. Serious Sam: The Random Encounter - Boss Theme</li>\n<li>27. Radical Fishing - Radical Fishing Theme</li>\n<li>28. GUN GODZ - Venus Supermax</li>\n<li>29. GUN GODZ - Sewers</li>\n<li>30. GUN GODZ - Boss Battle #1</li>\n<li>31. GUN GODZ - Basement</li>\n<li>32. GUN GODZ - Hotel Lobby</li>\n<li>33. GUN GODZ - Boss Battle #2 (Rooftop)</li>\n<li>34. Super Crate Box - Early Test</li>\n<li>35. Super Crate Box - Construction Yard (KOZILEK remix)</li>\n<li>36. GUNGODZ - Phonecall</li>\n</ul>', 6, 'Music', '../assets/img/vlambeer-ost-digital.jpg', '', 10),
-(8, 'Super Crate Box Re-arranged OST (Digital)', 'Super Crate Box soundtrack arranged with a band\n\n<ul class="list1">\n<li>1. Tutorial</li>										\n<li>2. Menu</li>\n<li>3. Construction yard</li>\n<li>4. Rocket silo</li>\n<li>5. Rest</li>\n<li>6. Moon temple</li>\n</ul>', 4, 'Music', '../assets/img/super-crate-box-ost.jpg', '', 10),
-(9, 'Luftrauser Limited Edition', 'Luxury LUFTRAUSERS edition incl. soundtrack, 3D printed airplane model. Only one made per LUFTRAUSERS build.', 4, 'Bundles', '../assets/img/luftrausers-limited-edition.jpg', '', 10);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_tempcarts`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_tempcarts` (
-  `tempcart_id` int(20) NOT NULL,
-  `product_id` int(20) NOT NULL,
-  `quantity` int(20) NOT NULL,
-  `item_price` float NOT NULL,
-  PRIMARY KEY (`tempcart_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `tbl_products` (`product_id`, `name`, `description`, `item_price`, `size`, `category`, `image`, `image2`, `stock`) VALUES
+(1, 'Luftrauser T-shirt', '\nStylish. Deadly. Beautiful. Strong.\nThese are all words that not only describe the masterful airship that is a Luftrauser ... but this shirt adorning the ships themselves!\nWhat is that rookie? You have never piloted a Luftrauser before? Pick up LUFTRAUSERS today on PC, Mac, PlayStation 3, PSVita or Linux!', 18, '', 'Clothes', '../assets/img/tshirt-luftrausers.jpg', '', 10),
+(2, 'Vlambeer T-shirt', 'T-shirt with Vlambeer logo on chest', 17.99, '', 'Clothes', '../assets/img/tshirt-vlambeer.jpg', '', 10),
+(3, 'Vlambeer 1st Birthday T-shirt', 'Dancing Vlambeer T-shirt', 18, '', 'Clothes', '../assets/img/tshirt-dancing-vlambeer.jpg', '', 10),
+(4, 'GUN GODZ T-shirt', 'GUN GODZ T-shirt with butt.', 18, '', 'Clothes', '../assets/img/tshirt-gungodz-butt.jpg', '', 10),
+(5, 'Vlambeer''s Crate Box Bundle', 'Includes t-shirt, exclusive poster, Vlambeer OST, pins, most of our 2010-2012 games on custom USB-drive', 50, '', 'Bundles', '../assets/img/crate-box-bundle.jpg', '', 10),
+(6, 'Vlambeer OST (Physical)', '\n		Few Indie Game Devs get their paws wet with as many composers and artists as Vlambeer does, and the logical conclusion is this, the Vlambeer Original Soundtrack. \n		Comprised of compositions from a variety of artists across a multitude of games, it’s the perfect collection for any gaming, music, or Vlambeer fan.\n	\n\n<ul class="list1">\n<li>1. GG Main Theme</li>										\n<li>2. RF Theme</li>\n<li>3. DZK Calm Seas</li>\n<li>4. DZK Menu Theme</li>\n<li>5. DZK Week #1</li>\n<li>6. DZK Week #2</li>\n<li>7. DZK Week #3</li>\n<li>8. DZK Night Theme</li>\n<li>9. LR Theme</li>\n<li>10. SCB Theme</li>\n<li>11. SCB Tutorial</li>\n<li>12. SCB Construction Ya</li>\n<li>13. SCB Rocket Silo</li>\n<li>14. SCB Moon Temple</li>\n<li>15. SCB Elevator Music</li>\n<li>16. LRS Demo 1</li>\n<li>17. LRS Demo 2</li>\n<li>18. LRS Demo 3</li>\n<li>19. SCB Interlude</li>\n</ul>\n<ul class="list2">\n<li>20. YH Day</li>\n<li>21. YH Night</li>\n<li>22. SS:TRE Menu Theme</li>\n<li>23. SS:TRE Desert Theme</li>\n<li>24. SS:TRE Temple Theme</li>\n<li>25. SS:TRE Pyramid Theme</li>\n<li>26. SS:TRE Boss Theme</li>\n<li>27. RF Radical Fishing</li>\n<li>28. GG Venus Supermax</li>\n<li>29. GG Sewers</li>\n<li>30. GG Boss Battle #1</li>\n<li>31. GG Basement</li>\n<li>32. GG Hotel Lobby</li>\n<li>33. GG Boss Battle #2</li>\n<li>34. SCB Early Test</li>\n<li>35. SCB Construction Yard (Kozilek Remix)</li>\n<li>36. GG Phonecall</li>\n</ul>', 12.99, '', 'Music', '../assets/img/vlambeer-ost-physical.jpg', '../assets/img/vlambeer-ost-physical-2.jpg', 10),
+(7, 'Vlambeer OST (Digital)', 'Includes unlimited streaming via the free Bandcamp app, plus high-quality download in MP3, FLAC and more.\n\n<ul class="list1">\n<li>1. GUN GODZ - Main Theme</li>										\n<li>2. Ridiculios Fishing - Theme</li>\n<li>3. Ridiculios Fishing - Calm Seas</li>\n<li>4. Dinosaur Zookeeper - Menu Theme</li>\n<li>5. Dinosaur Zookeeper - Week #1</li>\n<li>6. Dinosaur Zookeeper - Week #2</li>\n<li>7. Dinosaur Zookeeper - Week #3</li>\n<li>8. Dinosaur Zookeeper - Night Theme</li>\n<li>9. LUFTRAUSERS - Theme</li>\n<li>10. Super Crate Box - Theme</li>\n<li>11. Super Crate Box - Tutorial</li>\n<li>12. Super Crate Box - Construction Yard</li>\n<li>13. Super Crate Box - Rocket Silo</li>\n<li>14. Super Crate Box - Moon Temple</li>\n<li>15. Super Crate Box - Elevator Music</li>\n<li>16. LUFTRAUSERS - Demo #1</li>\n<li>17. LUFTRAUSERS - Demo #2</li>\n<li>18. LUFTRAUSERS - Demo #3</li>\n<li>19. Super Crate Box - Interlude</li>\n<li>20. Yeti Hunter - Day</li>\n<li>21. Yeti Hunter - Night</li>\n</ul>\n\n<ul class="list2">\n<li>22. Serious Sam: The Random Encounter - Menu Theme</li>\n<li>23. Serious Sam: The Random Encounter - Desert Theme</li>\n<li>24. Serious Sam: The Random Encounter - Temple Theme</li>\n<li>25. Serious Sam: The Random Encounter - Pyramide Theme</li>\n<li>26. Serious Sam: The Random Encounter - Boss Theme</li>\n<li>27. Radical Fishing - Radical Fishing Theme</li>\n<li>28. GUN GODZ - Venus Supermax</li>\n<li>29. GUN GODZ - Sewers</li>\n<li>30. GUN GODZ - Boss Battle #1</li>\n<li>31. GUN GODZ - Basement</li>\n<li>32. GUN GODZ - Hotel Lobby</li>\n<li>33. GUN GODZ - Boss Battle #2 (Rooftop)</li>\n<li>34. Super Crate Box - Early Test</li>\n<li>35. Super Crate Box - Construction Yard (KOZILEK remix)</li>\n<li>36. GUNGODZ - Phonecall</li>\n</ul>', 6, '', 'Music', '../assets/img/vlambeer-ost-digital.jpg', '', 10),
+(8, 'Super Crate Box Re-arranged OST (Digital)', 'Super Crate Box soundtrack arranged with a band\n\n<ul class="list1">\n<li>1. Tutorial</li>										\n<li>2. Menu</li>\n<li>3. Construction yard</li>\n<li>4. Rocket silo</li>\n<li>5. Rest</li>\n<li>6. Moon temple</li>\n</ul>', 4, '', 'Music', '../assets/img/super-crate-box-ost.jpg', '', 10),
+(9, 'Luftrauser Limited Edition', 'Luxury LUFTRAUSERS edition incl. soundtrack, 3D printed airplane model. Only one made per LUFTRAUSERS build.', 4, '', 'Bundles', '../assets/img/luftrausers-limited-edition.jpg', '', 10);
 
 -- --------------------------------------------------------
 
